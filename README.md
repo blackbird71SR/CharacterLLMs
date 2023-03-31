9b397fcc3d6e36be14dda25b0ee80ba30c6d7c2a
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-03-30 20:07:12 -0500

    Complete Neural Network model

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Models
 
 1. [Bigram Model](models/bigram.py)
+2. [Neural Network Model - v1](models/nn_v1.py)
--- models/nn_v1.py
+++ models/nn_v1.py
@@ -24,7 +24,73 @@ def createTrainingData(words):
   ys = torch.tensor(ys)
   return xs, ys
 
+def initalizeWeights(numChars):
+  g = torch.Generator().manual_seed(2147483647)
+  W = torch.randn((numChars, numChars), generator=g, requires_grad=True)
+  return W  
+
+def trainModel(xs, W, numChars, epochs):
+  for k in range(epochs):
+    # Forward Pass
+    xenc = F.one_hot(xs, num_classes=numChars).float() # Input to the network
+    logits = xenc @ W # Predict log-counts using current weights
+    counts = logits.exp() # Counts (non-neg) equivalent to N in bigram approach
+    probs = counts/ counts.sum(1, keepdim=True) # Probabs for next char by normalizing
+    loss = - probs[torch.arange(len(ys)), ys].log().mean() # Nomralized negative log-likelihood
+
+    # Backward Pass
+    W.grad = None
+    loss.backward()
+
+    # Update weights
+    W.data += -50 * W.grad
+
+    print(f'Epoch {k}: Loss {loss.data}')
+  return W
+
+def loss(examples, W, numChars):
+  xs, ys = createTrainingData(examples)
+  xenc = F.one_hot(xs, num_classes=numChars).float()
+  logits = xenc @ W
+  counts = logits.exp()
+  probs = counts/counts.sum(1, keepdim=True)
+  loss = - probs[torch.arange(len(ys)), ys].log().mean()
+  return loss
+
+def generateExample(W, g, itos, numChars):
+  out = []
+  ix = 0
+  while True:
+    xenc = F.one_hot(torch.tensor([ix]), num_classes=numChars).float()
+    logits = xenc @ W
+    counts = logits.exp()
+    probs = counts/counts.sum(1, keepdim=True)
+    ix = torch.multinomial(probs, num_samples=1, replacement=True, generator=g).item()
+    out.append(itos[ix])
+    if ix == 0:
+      break
+  return ''.join(out)
+
+def generateExamples(W, itos, numChars, numExamples=10):
+  g = torch.Generator().manual_seed(2147483647)
+  examples = []
+  for i in range(numExamples):
+    example = generateExample(W, g, itos, numChars)
+    examples.append(example)
+  return examples
 
 if __name__ == '__main__':
   words, stoi, itos, numChars = createWordsMapping()
   xs, ys = createTrainingData(words)
+  
+  W = initalizeWeights(numChars)
+  trainedW = trainModel(xs, W, numChars, epochs=20)
+  
+  examples = generateExamples(trainedW, itos, numChars)
+
+  train_loss = loss(words, trainedW, numChars)
+  test_loss = loss(examples, trainedW, numChars)
+
+  print(f'Generated Examples: {examples}')
+  print(f'Training Loss: {train_loss}')
+  print(f'Testing Loss: {test_loss}')
