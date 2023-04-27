53fa62960c00215c0c9a2405a6034471bcadcd6e
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-04-26 19:45:54 -0500

    Add 1D BatchNorm layer

--- models/nn_v3.py
+++ models/nn_v3.py
@@ -18,6 +18,38 @@ class Linear:
   def parameters(self):
     return [self.weight] + ([] if self.bias is None else [self.bias])
 
+class BatchNorm1d:
+
+  def __init__(self, dim, eps=1e-5, momentum=0.1):
+    self.eps = eps
+    self.momentum = momentum
+    self.training = True
+    # parameters (trained with backprop)
+    self.gamma = torch.ones(dim)
+    self.beta = torch.zeros(dim)
+    # buffers (trained with running 'momentum update')
+    self.running_mean = torch.zeros(dim)
+    self.running_var = torch.ones(dim)
+
+  def __call__(self, x):
+    if self.training:
+      xmean = x.mean(0, keepdims=True)
+      xvar = x.var(0, keepdims=True, unbiased=True)
+    else:
+      xmean = self.running_mean
+      xvar = self.running_var
+    
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+    self.out = self.gamma * xhat + self.beta
+
+    if self.training:
+      with torch.no_grad():
+        self.running_mean = (1-self.momentum) * self.running_mean + self.momentum * xmean
+        self.running_var = (1-self.momentum) * self.running_var + self.momentum * xvar
+    return self.out
+
+  def parameters(self):
+    return [self.gamma, self.beta]
 
 def createWordsMapping(filename = 'names.txt'):
   words = open(filename, 'r').read().splitlines()
