ccad956dc7e48b2e1d556f83ea11ab5d9490bc05
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-04-28 22:25:16 -0500

    Complete NN v3 model and training

--- models/nn_v3.py
+++ models/nn_v3.py
@@ -120,14 +120,92 @@ def initializeModelWeights(n_vocab, block_size, n_embed, n_hidden):
   print(f'Total Parameters: {sum(p.nelement() for p in parameters)}')
   return layers, parameters
 
+def trainModel(X, Y, layers, parameters, n_epochs, batch_size):
+  lossi = []
+
+  for epoch in range(n_epochs):
+
+    # Minibatch construct
+    ix = torch.randint(0, X.shape[0], (batch_size,), generator=g)
+    X_batch, Y_batch = X[ix], Y[ix]
+
+    # Forward Pass
+    emb = parameters[0][X_batch] # embed characters into vectors
+    x = emb.view(emb.shape[0], -1) # concatentae the vectors
+    for layer in layers:
+      x = layer(x)
+    loss = F.cross_entropy(x, Y_batch)
+
+    # Backward Pass
+    for layer in layers:
+      layer.out.retain_grad()
+    for p in parameters:
+      p.grad = None
+    loss.backward()
+
+    # Update parameters
+    lr = 0.1 if epoch < 100000 else 0.01 # stop learning rate decay
+    for p in parameters:
+      p.data += -lr * p.grad
+    
+    # Track Stats
+    lossi.append(loss.log10().item())
+    if epoch % 10000 == 0:
+      print(f'{epoch:7d}/{n_epochs:7d}: {loss.item():.4f}')
+    
+  return lossi, parameters
+
+@torch.no_grad()
+def loss(X, Y, layers, parameters):
+  emb = parameters[0][X]
+  x = emb.view(emb.shape[0], -1)
+  for layer in layers:
+    x = layer(x)
+  loss = F.cross_entropy(x, Y)
+  return loss
+
+def generateExample(layers, parameters, block_size, itos):
+  out = []
+  context = [0] * block_size
+  while True:
+    emb = parameters[0][torch.tensor([context])] # (1,block_size, d)
+    x = emb.view(emb.shape[0], -1)
+    for layer in layers:
+      if isinstance(layer, BatchNorm1d):
+        layer.training = False
+      x = layer(x)
+    probs = F.softmax(x, dim=1)
+    ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+    context = context[1:] + [ix]
+    out.append(ix)
+    if ix == 0:
+      break
+  return ''.join(itos[i] for i in out)
+
+def generateExamples(layers, parameters, block_size, itos, numExamples = 20):
+  examples = []
+  for _ in range(numExamples):
+    example = generateExample(layers, parameters, block_size, itos)
+    examples.append(example)
+  return examples
+
 if __name__ == '__main__':
   BLOCK_SIZE = 3
   N_EMBED = 10
-  N_HIDDEN = 200
+  N_HIDDEN = 100
   N_EPOCHS = 200000
   BATCH_SIZE = 32
   g = torch.Generator().manual_seed(2147483647)
 
   words, stoi, itos, n_vocab = createWordsMapping()
   Xtr, Ytr, Xdev, Ydev, Xte, Yte = buildDatasets(words, BLOCK_SIZE)
-  layers, parameters = initializeModelWeights(n_vocab, BLOCK_SIZE, N_EMBED, BATCH_SIZE)
+  layers, parameters = initializeModelWeights(n_vocab, BLOCK_SIZE, N_EMBED, N_HIDDEN)
+  lossi, parameters = trainModel(Xtr, Ytr, layers, parameters, N_EPOCHS, BATCH_SIZE)
+
+  plt.plot(lossi)
+
+  print(f'Train Loss: {loss(Xtr, Ytr, layers, parameters)}')
+  print(f'Val Loss: {loss(Xdev, Ydev, layers, parameters)}')
+
+  examples = generateExamples(layers, parameters, BLOCK_SIZE, itos)
+  print(f'Generated Examples: {examples}')
