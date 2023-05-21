26f741f57245addc7db86afa02dd76ccd9781097
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-05-21 11:47:36 -0500

    Add all the layer & model classes & initalize classes

--- models/nn_v4.py
+++ models/nn_v4.py
@@ -3,6 +3,110 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import random
 
+class Linear:
+
+  def __init__(self, fan_in, fan_out, bias=True):
+    self.weight = torch.randn((fan_in, fan_out), generator=g)/ fan_in ** 0.5
+    self.bias = torch.zeros(fan_out) if bias else None
+
+  def __call__(self, x):
+    self.out = x @ self.weight
+    if self.bias is not None:
+      self.out += self.bias
+    return self.out
+
+  def parameters(self):
+    return [self.weight] + ([] if self.bias is None else [self.bias])
+  
+class BatchNorm1d:
+
+  def __init__(self, dim, eps=1e-5, momentum=0.1):
+    self.eps = eps
+    self.momentum = momentum
+    self.training = True
+    # parameters (trined with backprop)
+    self.gamma = torch.ones(dim)
+    self.beta = torch.zeros(dim)
+    # buffers (trained with a running 'momentum update')
+    self.running_mean = torch.zeros(dim)
+    self.running_var = torch.ones(dim)
+
+  def __call__(self, x):
+    if self.training:
+      if x.ndim == 2:
+        dim = 0
+      elif x.ndim == 3:
+        dim = (0,1)
+      xmean = x.mean(dim, keepdims=True)
+      xvar = x.var(dim, keepdims=True, unbiased=True)
+    else:
+      xmean = self.running_mean
+      xvar = self.running_var
+    
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps) #normalize to unit variance
+    self.out = self.gamma * xhat + self.beta
+
+    if self.training:
+      with torch.no_grad():
+        self.running_mean = (1-self.momentum) * self.running_mean + self.momentum * xmean
+        self.running_var = (1-self.momentum) * self.running_var + self.momentum * xvar
+    
+    return self.out
+  
+  def parameters(self):
+    return [self.gamma, self.beta]
+  
+class Tanh:
+
+  def __call__(self, x):
+    self.out = torch.tanh(x)
+    return self.out
+
+  def parameters(self):
+    return []
+  
+class Embedding:
+
+  def __init__(self, num_embeddings, embedding_dim):
+    self.weight = torch.randn((num_embeddings, embedding_dim))
+  
+  def __call__(self, IX):
+    self.out = self.weight[IX]
+    return self.out
+
+  def parameters(self):
+    return [self.weight]
+  
+class FlattenConsecutive:
+  
+  def __init__(self, n):
+    self.n = n
+  
+  def __call__(self, x):
+    B,T,C = x.shape
+    x = x.view(B, T//self.n, C*self.n)
+    if x.shape[1] == 1:
+      x = x.squeeze(1)
+    self.out = x
+    return self.out
+  
+  def parameters(self):
+    return []
+  
+class Sequential:
+
+  def __init__(self, layers):
+    self.layers = layers
+  
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    self.out = x
+    return self.out
+  
+  def parameters(self):
+    return [p for layer in self.layers for p in layer.parameters()]
+
 def createWordsMapping(filename = 'names.txt'):
   words = open(filename, 'r').read().splitlines()
   chars = sorted(list(set(''.join(words))))
@@ -38,13 +142,36 @@ def buildDatasets(words, block_size):
 
   return Xtr, Ytr, Xdev, Ydev, Xte, Yte
 
+def initializeWeights(n_vocab, block_size, n_embed, n_hidden):
+  model = Sequential([
+    Embedding(n_vocab, n_embed),
+    FlattenConsecutive(2), Linear(n_embed*2, n_hidden, bias=False), BatchNorm1d(n_hidden),
+    Tanh(),
+    FlattenConsecutive(2), Linear(n_hidden*2, n_hidden, bias=False), BatchNorm1d(n_hidden),
+    Tanh(),
+    FlattenConsecutive(2), Linear(n_hidden*2, n_hidden, bias=False), BatchNorm1d(n_hidden),
+    Tanh(),
+    Linear(n_hidden, n_vocab)
+  ])
+  
+  with torch.no_grad():
+    model.layers[-1].weight *= 0.1 # Make last lyer less confident
+
+  parameters = model.parameters()
+  for p in parameters:
+    p.requires_grad = True
+  
+  print(f'Total Parameters: {sum(p.nelement() for p in parameters)}')
+  return model
+
 if __name__ == '__main__':
-  BLOCK_SIZE = 3
-  N_EMBED = 10
-  N_HIDDEN = 100
+  BLOCK_SIZE = 8
+  N_EMBED = 24
+  N_HIDDEN = 128
   N_EPOCHS = 200000
   BATCH_SIZE = 32
   g = torch.Generator().manual_seed(2147483647)
 
   words, stoi, itos, n_vocab = createWordsMapping()
   Xtr, Ytr, Xdev, Ydev, Xte, Yte = buildDatasets(words, BLOCK_SIZE)
+  model = initializeWeights(n_vocab, BLOCK_SIZE, N_EMBED, N_HIDDEN)
