790eb73e16f835c05328988b18f1994c99372475
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-06-18 13:07:37 -0500

    Add Self Attention Head

--- nano_gpt_models/v2.py
+++ nano_gpt_models/v2.py
@@ -48,6 +48,31 @@ def getBatch(data):
   x, y = x.to(device), y.to(device)
   return x,y
 
+class Head(nn.Module):
+  """one head of self-attention"""
+
+  def __init__(self, head_size):
+    super().__init__()
+    self.key = nn.Linear(n_embed, head_size, bias=False)
+    self.query = nn.Linear(n_embed, head_size, bias=False)
+    self.value = nn.Linear(n_embed, head_size, bias=False)
+    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+  def forward(self, x):
+    B,T,C = x.shape
+    k = self.key(x)     # (B,T,C)
+    q = self.query(x)   # (B,T,C)
+    
+    # compute attention scores ("affinities")
+    wei = q @ k.transpose(-2, -1) * (C ** -0.5) # (B, T, C) @ (B, C, T) -> (B, T, T)
+    wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+    wei = F.softmax(wei, dim=-1) # (B, T, T)
+
+    # perform the weighted aggrgaetion of the values
+    v = self.value(x) # (B, T, C)
+    out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+    return out
+
 
 class BigramLM(nn.Module):
   
